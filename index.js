211ea5bd948d6993f1e4081f72b18c512d98140e
Esteban Palacio <estipalacio@gmail.com>
2021-06-18 16:01:29 -0400

    added better end-results display by mapping through planet names, and better filtering for habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+//using the npm package csv-parse
 const parse = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+//create an array to store all filtered info.
+const habitablePlanets = [];
+
+//creating a function that filters the specific csv file for the column 'koi_disposition' to see if the exoplanet is confirmed.
+function isHabitablePlanet(planet) {
+    //checking if planet is confirmed
+    return planet['koi_disposition'] === 'CONFIRMED'
+    //checking if planet is within temperature range for habitability
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    //checking for the upper limit of planet size radius
+    && planet['koi_prad'] < 1.6;
+};
 
 fs.createReadStream('kepler_data.csv')
     //pipe connects a readable stream source to a writable stream destination
@@ -13,7 +25,9 @@ fs.createReadStream('kepler_data.csv')
     }))
     //reading the data
     .on('data', (data) => {
-        results.push(data);
+        if(isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     //throwing an error in case there is one
     .on('error', (err) => {
@@ -21,8 +35,10 @@ fs.createReadStream('kepler_data.csv')
     })
     //what to do on completion
     .on('end', () => {
-        console.log(results);
-        console.log('done');
+        //mapping the array to extract they key-values of the planet names.
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }));
+        //logging the amount of elements inside the array
+        console.log(`${habitablePlanets.length} habitable planets found!`);
     });
-
-// parse();
