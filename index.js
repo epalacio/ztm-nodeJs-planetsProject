2f39170d1e69a376275c4dfecfe524ddb843cd22
Esteban Palacio <estipalacio@gmail.com>
2021-06-18 15:35:11 -0400

    added main function to read stream

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const parse = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+    //reading the data
+    .on('data', (data) => {
+        results.push(data);
+    })
+    //throwing an error in case there is one
+    .on('error', (err) => {
+        console.log(err);
+    })
+    //what to do on completion
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
+
+// parse();
